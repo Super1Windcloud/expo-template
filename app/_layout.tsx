35870aba2a59dc16f4de0d7711e5e2259c1c2153
SuperWindCloud <19338823723@163.com>
2025-11-03 20:32:57 +0800

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,14 +2,17 @@ import '../global.css'
 import '@/locales'
 import { PortalHost } from '@rn-primitives/portal'
 import { Stack } from 'expo-router'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import { Theme } from '@/components/theme'
 
 export default function Layout() {
   return (
-    <Theme>
-      <Stack />
-      <PortalHost />
-    </Theme>
+    <SafeAreaProvider>
+      <Theme>
+        <Stack />
+        <PortalHost />
+      </Theme>
+    </SafeAreaProvider>
   )
 }
